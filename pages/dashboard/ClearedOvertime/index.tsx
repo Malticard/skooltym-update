fe975dacf40339bdc699a69f2402ddf650e55129
bruno <brunohectre@gmail.com>
2025-07-21 23:00:54 +0300

    removed un used import

--- pages/dashboard/ClearedOvertime/index.tsx
+++ pages/dashboard/ClearedOvertime/index.tsx
@@ -1,4 +1,3 @@
-import { OvertimeModel } from '@/interfaces/OvertimeModel';
 import PageHeader from '@/shared/layout-components/page-header/page-header';
 import Seo from '@/shared/layout-components/seo/seo';
 import { fetchClearedOvertime } from '@/utils/data_fetch';
